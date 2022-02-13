1506e8db7c4feec8403027ac56ef4f170763b390
arielmazor <ariel.mazor.mail@gmail.com>
2022-02-13 21:53:39 +0200

    ...

---
+++ bin/cli.js
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+
+const { execSync } = require('child_process');
+
+const runCommand = command =>{
+  try{
+    execSync(`${command}, {stdio:'inherit'}`);
+  } catch(e) {
+    console.error(`Failed to execute ${command}`, e);
+    return false;
+  }
+  return true;
+};
+
+const repoName = process.argv[2];
+const gitCheckoutCommand = `git clone --depth 1 ${repoName}`;
+const installDepsCommand = `cd ${repoName} && npm install`;
+
+console.log(`cloning the repo with name ${repoName}`);
+const checkedOut = runCommand(gitCheckoutCommand);
+if(!checkedOut) process.exit(code:-1);
+
+console.log(`Installing dependencies for ${repoName}`);
+const installDeps = runCommand(installDepsCommand);
+if(!installDeps) process.exit(code:-1);
+
+console.log("Congratulations! You are ready to Code. Follow the following commands to start");
+console.log(`cd ${repoName} && npm start`);
